beca2677485cae44217460097c029c1d47e5ebec
moonR2 <moonyneverend@gmail.com>
2023-06-25 16:18:29 -0500

    Mentions for users in basic commands

--- bot/commands/basics.py
+++ bot/commands/basics.py
@@ -11,8 +11,18 @@ class Basics(commands.Cog):
 
     @commands.command()
     async def jaime(self, ctx):
-        await ctx.send('Jaime alias (Jastk45) es un jugador main Diana, que se dedica a trolear partidas en MID. También de vez en cuando se dedica a jugar TFT pero el Jacko es mejor.')
+        user = await self.bot.fetch_user('703721870482079834')
+        await ctx.send(f'{user.mention} es un jugador main Diana, que se dedica a trolear partidas en MID. También de vez en cuando se dedica a jugar TFT pero el Jacko es mejor.')
 
+    @commands.command()
+    async def microrocket(self, ctx):
+        user = await self.bot.fetch_user('249434388167655435')
+        await ctx.send(f'{user.mention} alias MarcoManco es el mejor jugador de Volibear en todo el mundo. A veces también juega TFT pero es un manco de mierda.')
+
+    @commands.command()
+    async def stiwers(self, ctx):
+        user = await self.bot.fetch_user('548529415055998988')
+        await ctx.send(f'{user.mention} es un manco de mierda, no hace falta decir mas.')
 
 async def setup(bot):
     await bot.add_cog(Basics(bot))
